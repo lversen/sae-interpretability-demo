8e3f8feb48c83e0cca231e864392668743436b7d
lversen <98595238+lversen@users.noreply.github.com>
2024-07-03 15:32:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,5 @@ n = 5000
 datasets = ["data\\" + dataset for dataset in os.listdir("data")]
 models = ["Alibaba-NLP/gte-large-en-v1.5", 'whaleloops/phrase-bert', "paraphrase-MiniLM-L6-v2", "all-mpnet-base-v2"]
 model_dict = run_all(datasets, models, n, graph=True)
+
+#Try to implement similar structure to movie datasets
