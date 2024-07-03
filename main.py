084b3cd6c84aaa8f531574cfc9bb5928b87433f9
lversen <98595238+lversen@users.noreply.github.com>
2024-07-03 13:37:19 +0200

    Update

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 import os
 from gephi_export import *
 n = 5000
-#gephi_export("data\stock_data.csv", 'whaleloops/phrase-bert', "Text", n)
 
 datasets = ["data\\" + dataset for dataset in os.listdir("data")]
 models = ['whaleloops/phrase-bert', "paraphrase-MiniLM-L6-v2", "all-mpnet-base-v2"]
