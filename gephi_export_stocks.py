d951e1b97b5210fc43d659208c0e2ddd93dd0d48
lversen <98595238+lversen@users.noreply.github.com>
2024-07-05 11:31:46 +0200

    Making the same subset usuable for different models

--- gephi_export_stocks.py
+++ gephi_export_stocks.py
@@ -12,44 +12,60 @@ def batch_encode(model, feature_list, batch_size=16):
         batch_embeddings = model.encode(batch, device='cuda')  # Specify device
         embeddings.extend(batch_embeddings)
         torch.cuda.empty_cache()  # Clear cache after each batch
-        print(i)
     return np.array(embeddings)
 
-def preprocess(df, n):
-    lengths = np.array([])
-    for c in df:
-        if type(df[c][0]) is str:
-            length = df[c].str.len().sum()
-            lengths = np.append(lengths, length)
-        else:
-            lengths = np.append(lengths, 0)
-    largest = np.where(lengths == np.max(lengths))[0][0]
-    data_name = df.columns[largest]
-    l = list(df.columns)
-    l.remove(data_name)
-    l = np.append([data_name], l)
-    df = df[l]
-    rows = np.random.choice(df.index, size=n, replace=False)
-    df = df.iloc[rows]
-    df = df.reset_index()
-    df = df.drop(columns="index")
-    df[df.columns[0]] = df[df.columns[0]].str.strip()
+def preprocess(df, content_column, dataset_iteration, n):
+    if len(content_column) != 0:
+        data_name = content_column[dataset_iteration]
+        l = list(df.columns)
+        l.remove(data_name)
+        l = np.append([data_name], l)
+        df = df[l]
+        rows = np.random.choice(df.index, size=n, replace=False)
+        df = df.iloc[rows]
+        df = df.reset_index()
+        df = df.drop(columns="index")
+        df[df.columns[0]] = df[df.columns[0]].str.strip()
+        return(df)
+    else:
+        lengths = np.array([])
+        for c in df:
+            if type(df[c][0]) is str:
+                length = df[c].str.len().sum()
+                lengths = np.append(lengths, length)
+            else:
+                lengths = np.append(lengths, 0)
+        largest = np.where(lengths == np.max(lengths))[0][0]
+        data_name = df.columns[largest]
+        l = list(df.columns)
+        l.remove(data_name)
+        l = np.append([data_name], l)
+        df = df[l]
+        rows = np.random.choice(df.index, size=n, replace=False)
+        df = df.iloc[rows]
+        df = df.reset_index()
+        df = df.drop(columns="index")
+        df[df.columns[0]] = df[df.columns[0]].str.strip()
+        return(df)
+
+def data_frame_init(file_name, content_column, dataset_iteration, n):
+    df = pd.read_csv(file_name, encoding = "ISO-8859-1")
+    df = preprocess(df, content_column,  dataset_iteration, n)
     return(df)
 
-
-def feature_extraction(file_name, model_name, n, batch_size=16, iterations=1):
-    print(file_name)
-    df = pd.read_csv(file_name, encoding = "ISO-8859-1")
-    df = preprocess(df, n)
+def feature_extraction(df, file_name, model_name, dataset_iteration, batch_size, iterations, title_column):
+    title_column = np.array(title_column)
     data, data_attributes = df[df.columns[0]], df[df.columns[1:]]
-
     feature_dict = {}
     mapping = {}
     attributes = {}
     for j in range(iterations):
         for i, text in enumerate(data):
             feature_dict[i] = text
-            mapping[i + len(data)*j] = str(i + len(data)*j) + file_name + "_" + model_name + "_" + str(j)
+            if len(title_column) == 0:
+                mapping[i + len(data)*j] = str(i + len(data)*j) + file_name + "_" + model_name + "_" + str(j)
+            else:
+                mapping[i + len(data)*j] = df[title_column[dataset_iteration]][i + len(data)*j]
             attributes[mapping[i + len(data)*j]] = dict(zip(list(data_attributes.columns), [attribute[i] for attribute in data_attributes.to_numpy().T]))
             attributes[mapping[i + len(data)*j]]["Model Name"] = model_name
     model = SentenceTransformer(model_name, trust_remote_code=True)
@@ -71,25 +87,34 @@ def gephi(feature_extract, file_name, model_name, mapping, attributes):
 
     G = nx.DiGraph(nn)
     H = nx.relabel_nodes(G, mapping)
+    print(list(H.nodes))
     nx.set_node_attributes(H, attributes)
     nx.write_gexf(H, file_name + "_" +  model_name + ".gexf")
 
-def gephi_export(feature_extract, file_name, model_name, mapping, attributes, n):
+def gephi_export(feature_extract, file_name, model_name, mapping, attributes):
     file_name = file_name.replace(".csv", "")
     file_name = file_name.replace("data\\", "")
     model_name = model_name.replace("/", "_")
     gephi(feature_extract, file_name, model_name, mapping, attributes)
 
-def run_all(datasets, models, n, graph=False, batch_size=16, iterations=1):
+def run_all(datasets, models, n, graph=False, batch_size=16, iterations=1, content_column=[], title_column=[]):
     model_dict = {}
-    for model in models:
-        print(model)
-        for i, dataset in enumerate(datasets):
-            if i == 0:
-                feature_extract, mapping, attributes = feature_extraction(dataset, model, n, batch_size, iterations)
-                if graph == True: gephi_export(feature_extract, dataset, model, mapping, attributes, n)
-            else:
-                feature_extract = np.dstack((feature_extract, feature_extraction(dataset, model, n, batch_size, iterations)[0]))
-                if graph == True: gephi_export(feature_extract, dataset, model, mapping, attributes, n)
-        model_dict[model] = feature_extract
+    for i, dataset in enumerate(datasets):
+        df = data_frame_init(dataset, content_column, i, n)
+
+        for j, model in enumerate(models):
+            print(model)
+            feature_extract, mapping, attributes = feature_extraction(df, dataset, model, i, batch_size, iterations, title_column)
+            if graph == True: gephi_export(feature_extract, dataset, model, mapping, attributes)
+        
+        if len(title_column) != 0:
+            title_column = np.delete(title_column, i)
+        if len(content_column) != 0:
+            content_column = np.delete(content_column, i)
     return(model_dict)
+"""  else:
+                feature_extract = np.dstack((feature_extract, feature_extraction(df, dataset, model, i, batch_size, iterations, title_column)[0]))
+                print(feature_extract.shape)
+                if graph == True: gephi_export(feature_extract, dataset, model, mapping, attributes, n)
+ """
+            #model_dict[model] = feature_extract
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import os
 from gephi_export_stocks import *
-n = 9470 # length of tweet dataset
+n = 10
 models = ["BAAI/bge-m3", "intfloat/e5-large-v2", "Alibaba-NLP/gte-large-en-v1.5", 'whaleloops/phrase-bert', "paraphrase-MiniLM-L6-v2", "all-mpnet-base-v2"]
 
-#run_all(["data_movies\\final_data.csv"], models, n, graph=True, batch_size=16)
+run_all(["data_movies\\final_data.csv"], models, n, graph=True, batch_size=16, content_column=["Description"], title_column=["Name"])
 
 #datasets = ["data\\" + dataset for dataset in os.listdir("data")]
-datasets = ["data\\Project6500.csv"]
-model_dict = run_all(datasets, models, n, graph=True, batch_size=32, iterations=1)
+#datasets = ["data\\Project6500.csv"]
+#model_dict = run_all(datasets, models, n, graph=True, batch_size=32, iterations=1)
