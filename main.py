67a4f3e2c0bed17a7eb35cacd1b536c3c697b903
lversen <98595238+lversen@users.noreply.github.com>
2024-07-06 10:22:56 +0200

    Tried large dataset with language classification

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 from gephi_export_stocks import *
-n = 500
+n = 10_000
 #models = ["BAAI/bge-m3", "intfloat/e5-large-v2", "Alibaba-NLP/gte-large-en-v1.5", 'whaleloops/phrase-bert', "paraphrase-MiniLM-L6-v2", "all-mpnet-base-v2"]
 models = ["Alibaba-NLP/gte-large-en-v1.5"]
 run_all(["data_movies\\final_data.csv"], models, n, graph=True, batch_size=16, content_column=["Description"], title_column=["Name"], language_classes=True)
@@ -16,3 +16,6 @@ run_all(["data_movies\\final_data.csv"], models, n, graph=True, batch_size=16, c
 # TRY TRANSLATING MOVIE DESCRIPTIONS TO ENGLISH
 #https://python.langchain.com/v0.2/docs/integrations/platforms/huggingface/
 #USE BGE MODEL
+# TRY OPENAI ADA EMBEDDINGS from langchain.embeddings import OpenAIEmbeddings
+
+#94 percent of languages were english all of the sudden at n=10_000
