d81c1093093828158996523c1ce78aa24d2014dd
Sebastian Iversen <siv017@uit.no>
2024-07-08 15:13:11 +0200

    Adding language classification to file
    If existing language is found it does not have to be classified

--- gephi_export_stocks.py
+++ gephi_export_stocks.py
@@ -5,30 +5,56 @@ import numpy as np
 import pandas as pd
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
-def language_classifier(df, columns, file_name):
+import sys
+np.set_printoptions(threshold=sys.maxsize, suppress=False)
+
+def language_classifier(df, rows, max_rows, columns, file_name):
     model_name = 'qanastek/51-languages-classifier'
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     classifier = TextClassificationPipeline(model=model, tokenizer=tokenizer, device="cuda")
 
+    df_max = pd.read_csv(file_name, encoding = "ISO-8859-1")
+
     for c in columns:
-        if c in df.columns:
-            print("Classifying languages from '" + c + "'")
-            data = classifier(list(df[c].to_numpy()))
-            data = [d["label"] for d in data]
-            c += "_classified"
-            df[c] = data
-        else: raise ValueError(c + " is not a column in " + file_name)
+        c_classified = c + "_classified"
+        if c_classified not in df.columns:    
+            if c in df.columns:
+                print("Classifying languages from '" + c + "'")
+                data = classifier(list(df[c].to_numpy()))
+                data = [d["label"] for d in data]
+                df[c_classified] = data
+
+                data_max = np.zeros(max_rows, dtype='U100')
+                data_max[np.arange(max_rows)] = "empty"
+                data_max[rows] = data
+                df_max[c_classified] = data_max
+                df_max.to_csv("data_movies\\final_data.csv")
+            else: raise ValueError(c + " is not a column in " + file_name)
+        else:
+            rows_remaining = np.array([])
+            data = np.array([], dtype='U100')
+            for r in range(len(df)):
+                d_point = df.loc[r][c_classified]
+                if d_point == "empty":
+                    rows_remaining = np.append(rows_remaining, r)
+                    data = np.append(data, classifier(df))
+                else:
+                    data = np.append(data, d_point)
+            print(rows_remaining)
+            
+            
 
 
-
-def batch_encode(model, feature_list, batch_size=16):
+def batch_encode(model, feature_list, batch_size, n):
     embeddings = []
     for i in range(0, len(feature_list), batch_size):
         batch = feature_list[i:i+batch_size]
         batch_embeddings = model.encode(batch, device='cuda')
         embeddings.extend(batch_embeddings)
-        print(str(i) + " encoding")
+        if i>0:
+            if int(i*100/n) > int((i-1)*100/n):
+                print(str(int(i*100/n)) + "%" + " encoding")
         torch.cuda.empty_cache()
     return np.array(embeddings)
 
@@ -49,11 +75,12 @@ def preprocess(df, file_name, content_column, dataset_iteration, n):
             l = np.append([data_name], l)
             df = df[l]
             rows = np.random.choice(df.index, size=n, replace=False)
+            max_rows = len(df)
             df = df.iloc[rows]
             df = df.reset_index()
             df = df.drop(columns="index")
             df[df.columns[0]] = df[df.columns[0]].str.strip()
-            return(df)
+            return(df, rows, max_rows)
         else: raise ValueError(data_name + " is not a column in " + file_name)
     else:
         lengths = np.array([])
@@ -70,16 +97,17 @@ def preprocess(df, file_name, content_column, dataset_iteration, n):
         l = np.append([data_name], l)
         df = df[l]
         rows = np.random.choice(df.index, size=n, replace=False)
+        max_rows = len(df)
         df = df.iloc[rows]
         df = df.reset_index()
         df = df.drop(columns="index")
         df[df.columns[0]] = df[df.columns[0]].str.strip()
-        return(df)
+        return(df, rows, max_rows)
 
 def data_frame_init(file_name, content_column, dataset_iteration, n):
     df = pd.read_csv(file_name, encoding = "ISO-8859-1")
-    df = preprocess(df, file_name, content_column,  dataset_iteration, n)
-    return(df)
+    df, rows, max_rows = preprocess(df, file_name, content_column,  dataset_iteration, n)
+    return(df, rows, max_rows)
 
 def node_attributes(df, file_name, dataset_iteration, title_column):
     if title_column[dataset_iteration] in df.columns:
@@ -89,11 +117,11 @@ def node_attributes(df, file_name, dataset_iteration, title_column):
         return(mapping, attributes)
     else: raise ValueError(title_column[dataset_iteration] + " is not a column in " + file_name)
 
-def feature_extraction(df, model_name, batch_size):
+def feature_extraction(df, model_name, batch_size, n):
     feature_list = df[df.columns[0]].to_numpy()
     model = SentenceTransformer(model_name, trust_remote_code=True)
     with torch.no_grad():
-        feature_extract = batch_encode(model, feature_list, batch_size)
+        feature_extract = batch_encode(model, feature_list, batch_size, n)
     print(np.shape(feature_extract))
     return(feature_extract)
 
@@ -117,13 +145,13 @@ def run_all(datasets, models, n, graph=False, batch_size=16, content_column=[], 
     model_dict = {}
     for i, dataset in enumerate(datasets):
         print(dataset)
-        df = data_frame_init(dataset, content_column, i, n)
+        df, rows, max_rows = data_frame_init(dataset, content_column, i, n)
         if len(classify_language) != 0:
-            language_classifier(df, classify_language, dataset)
+            language_classifier(df, rows, max_rows, classify_language, dataset)
             mapping, attributes = node_attributes(df, dataset, i, title_column)
 
         for j, model in enumerate(models):
-            feature_extract = feature_extraction(df, model, batch_size)
+            feature_extract = feature_extraction(df, model, batch_size, n)
             if graph == True: gephi_export(feature_extract, dataset, model, mapping, attributes)
         
         if len(title_column) != 0:
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 import os
 from gephi_export_stocks import *
-n = 10_000
-models = ["BAAI/bge-m3", "intfloat/e5-large-v2", "Alibaba-NLP/gte-large-en-v1.5", 'whaleloops/phrase-bert', "paraphrase-MiniLM-L6-v2", "all-mpnet-base-v2"]
-#models = ["BAAI/bge-m3"]
-run_all(["data_movies\\final_data.csv"], models, n, graph=True, batch_size=16,
+n = 10
+#models = ["BAAI/bge-m3", "intfloat/e5-large-v2", "Alibaba-NLP/gte-large-en-v1.5", 'whaleloops/phrase-bert', "paraphrase-MiniLM-L6-v2", "all-mpnet-base-v2"]
+models = ["BAAI/bge-m3"]
+run_all(["data_movies\\final_data.csv"], models, n, graph=False, batch_size=16,
         content_column=["Description"], title_column=["Name"], classify_language=["Name"])
 
 #datasets = ["data\\" + dataset for dataset in os.listdir("data")]
