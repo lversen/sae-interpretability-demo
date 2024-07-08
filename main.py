d4879c919addd09e3a7eeaccaacfea35ab3a00f1
Sebastian Iversen <siv017@uit.no>
2024-07-08 09:16:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 import os
 from gephi_export_stocks import *
-n = 10_000
+n = 10
 #models = ["BAAI/bge-m3", "intfloat/e5-large-v2", "Alibaba-NLP/gte-large-en-v1.5", 'whaleloops/phrase-bert', "paraphrase-MiniLM-L6-v2", "all-mpnet-base-v2"]
 models = ["Alibaba-NLP/gte-large-en-v1.5"]
-run_all(["data_movies\\final_data.csv"], models, n, graph=True, batch_size=16, content_column=["Description"], title_column=["Name"], language_classes=True)
+run_all(["data_movies\\final_data.csv"], models, n, graph=True, batch_size=16, content_column=["Description"], title_column=["Name"])
 
 #datasets = ["data\\" + dataset for dataset in os.listdir("data")]
 #datasets = ["data\\Project6500.csv"]
@@ -17,5 +17,3 @@ run_all(["data_movies\\final_data.csv"], models, n, graph=True, batch_size=16, c
 #https://python.langchain.com/v0.2/docs/integrations/platforms/huggingface/
 #USE BGE MODEL
 # TRY OPENAI ADA EMBEDDINGS from langchain.embeddings import OpenAIEmbeddings
-
-#94 percent of languages were english all of the sudden at n=10_000
